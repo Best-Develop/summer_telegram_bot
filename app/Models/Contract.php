2af47063db6ad6493a6b0fa622e2f2589983063f
OzodbekCoder <ergashevozodbek16@gmail.com>
2024-05-14 23:44:24 +0500

    set begin and end date action bot

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Contract extends Model
 {
     use HasFactory;
+    const BEGIN_DATE = '2024-05-15';
+    const END_DATE = '2024-08-31';
     public function summer_action_gift_player()
     {
         return $this->belongsTo(SummerActionGiftPlayer::class, 'id', 'contract_id');
